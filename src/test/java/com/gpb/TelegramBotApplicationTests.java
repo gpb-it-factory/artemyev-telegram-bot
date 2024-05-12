39e74fa1193a468c89147a900f009ddcc3f10c9d
Nickolay Artemyev <Artemyev1000@gmail.com>
2024-05-12 21:05:02 +0300

    Задание 3. Добавил Spring Boot и Gradle

---
+++ src/test/java/com/gpb/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleGpbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
