c37082d2ab7caae4d915e1bd56e34ff3f35fdebc
Nickolay Artemyev <Artemyev1000@gmail.com>
2024-05-19 22:29:21 +0300

    Задание 3. Создал "скелет" бота. Исправлено 3.

--- src/main/java/com/gpb/strategy/StartCommandStrategy.java
+++ src/main/java/com/gpb/strategy/StartCommandStrategy.java
@@ -6,6 +6,6 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 public final class StartCommandStrategy implements CommandProcessingStrategy {
     @Override
     public String process(Message message) {
-        return String.format(BotMessage.ANSWER_MESSAGE.getText(), message.getChat().getFirstName());
+        return String.format(BotMessage.WELCOME_MESSAGE.getText(), message.getChat().getFirstName());
     }
 }
